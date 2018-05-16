de209d80d25243627fe1bd012554a9aef17311b8
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-05-16 16:26:47 +0300

    Hash

---
+++ modulo1/hash/hash.rb
@@ -0,0 +1,8 @@
+# hash chaveado com string
+hash_string = { "nome" => "Eliza", "idade" => "29"}
+
+#hash com symbol
+hash_symbol = {name: "Eliza", age: "29"}
+
+puts hash_symbol[:name]
+puts hash_symbol[:age]
